39b8191818844b6c7617bfb256d92749d13d6661
Moni-Chaurasiya <monichaurasiya993@gmail.com>
2025-09-12 10:34:58 +0530

    feat: Backend file setup and schema created

---
+++ backend/models/Transaction.js
@@ -0,0 +1,33 @@
+const mongoose=require('mongoose');
+
+const transactionSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    title:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    amount:{
+        type:Number,
+        required :true
+    },
+    category:{
+        type:String,
+        required:true,
+        enum:['Food','Transport','Entertainment','Shopping','Bills','Salary','Other']
+    },
+    type:{
+        type:String,
+        enum:['income','expense'],
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+})
+module.exports=mongoose.model('Transaction',transactionSchema)
